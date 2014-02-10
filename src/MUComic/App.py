f4f4d60f9ad8a10aaa7c7e4d8c82f0ce5fb6c8a8
Christoph Stahl <christoph.stahl@uni-dortmund.de>
2014-02-10 16:33:18 +0100

    makedirs for windows

--- src/MUComic/App.py
+++ src/MUComic/App.py
@@ -3,6 +3,6 @@ import os
 
 app = AppDirs('MUComicLoad', 'OpenSomething.Org')
 if not os.path.isdir(app.user_data_dir):
-	os.mkdir(app.user_data_dir)
+	os.makedirs(app.user_data_dir)
 if not os.path.isdir(os.path.join(app.user_data_dir,'covers')):
-	os.mkdir(os.path.join(app.user_data_dir, 'covers'))
+	os.makedirs(os.path.join(app.user_data_dir, 'covers'))
