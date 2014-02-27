d3dbbf8e393b75de634ccf1262f5e90e3066e130
Christoph Stahl <christoph.stahl@uni-dortmund.de>
2014-02-27 17:40:03 +0100

    other icons themed

--- src/mucomic/Qt/icons.py
+++ src/mucomic/Qt/icons.py
@@ -7,4 +7,12 @@ def addThemedIcon(action, name):
 
 def changeIconsUI(ui):
 	addThemedIcon(ui.menuFileSettings, 'preferences-system')
+	addThemedIcon(ui.menuFileQuit, 'system-log-out')
 	addThemedIcon(ui.actionAdd_series, 'list-add')
+	addThemedIcon(ui.actionRemove_series, 'list-remove')
+	addThemedIcon(ui.actionUpdate, 'view-refresh')
+	addThemedIcon(ui.actionDownload_Issue, 'go-bottom')
+	addThemedIcon(ui.actionDownload_Series, 'go-bottom')
+	addThemedIcon(ui.actionDownload, 'go-bottom')
+	addThemedIcon(ui.actionOpen, 'edit-find')
+	addThemedIcon(ui.actionFav_series, 'emblem-favourite')
