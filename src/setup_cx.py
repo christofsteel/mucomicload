2ecec2d554ee8c18ed5e6879772452bb4c81327b
Christoph Stahl <christoph.stahl@uni-dortmund.de>
2014-05-15 00:08:01 +0200

    Additional setup.py

--- src/setup_cx.py
+++ src/setup_cx.py
@@ -1,9 +1,14 @@
-from distutils.core import setup
+from cx_Freeze import setup, Executable
 
 # Dependencies are automatically detected, but it might need
 # fine tuning.
 
 import sys
+base = 'Win32GUI' if sys.platform=='win32' else None
+
+executables = [
+	Executable('mucomicload', base=base)
+]
 
 setup(name='MUComicLoad',
 	author = "Christoph \"Hammy\" Stahl",
@@ -13,4 +18,5 @@ setup(name='MUComicLoad',
 	requires=['pyside', 'appdirs'],
 	scripts=['mucomicload'],
 	version = '2.0',
-	description = 'Python downloader for Marvel Unlimited comic subscription')
+	description = 'Python downloader for Marvel Unlimited comic subscription',
+	executables = executables)
